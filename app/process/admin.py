470f7c48824ab7c1d218fa51ef25cbe442187fa2
Eduardo <eduhenriquesrs@gmail.com>
2022-09-30 19:43:53 -0300

    Criado models de Partes do Processo.

--- app/process/admin.py
+++ app/process/admin.py
@@ -2,10 +2,25 @@ from django.contrib import admin
 from .models import *
 
 
+class PartInline(admin.StackedInline):
+    model = Part
+    extra = 0
+
+
 @admin.register(Process)
 class ProcessAdmin(admin.ModelAdmin):
-    list_display = ['subject', 'judge']
-    search_fields = ['subject', 'judge']
-    list_display_links = ['subject', 'judge']
+    list_display = ['number', 'subject', 'judge']
+    search_fields = ['number', 'subject', 'judge']
+    list_display_links = ['number', 'subject', 'judge']
     ordering = ('number',)
     list_filter = ('judge',)
+    inlines = [PartInline, ]
+
+
+@admin.register(Part)
+class PartAdmin(admin.ModelAdmin):
+    list_display = ['name', 'category']
+    search_fields = ['name', 'category']
+    list_display_links = ['name', 'category']
+    ordering = ('name',)
+    list_filter = ('category',)
--- app/process/models.py
+++ app/process/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+import uuid
 
 
 class Process(models.Model):
-    number = models.IntegerField(verbose_name="Number of Process", unique=True)
+    number = models.UUIDField(verbose_name="Number of Process", default=uuid.uuid4, editable=False, unique=True)
     department = models.CharField(verbose_name="Class", max_length=50)
     subject = models.CharField(verbose_name="Subject", max_length=50)
     judge = models.CharField(verbose_name="Judge", max_length=50)
@@ -13,3 +14,16 @@ class Process(models.Model):
     class Meta:
         verbose_name = "Process"
         verbose_name_plural = "Processes"
+
+
+class Part(models.Model):
+    name = models.CharField(verbose_name="Name", max_length=100)
+    category = models.CharField(verbose_name="Category", max_length=50)
+    process = models.ForeignKey(Process, verbose_name="Process", on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Part"
+        verbose_name_plural = "Parts"
