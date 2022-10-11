cf0951a716647b216b60502bf25b2fd4024bd1dd
Eduardo <eduhenriquesrs@gmail.com>
2022-10-10 22:58:36 -0300

    added default to DEBUG.

--- desafioRetake/settings.py
+++ desafioRetake/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECRET_KEY = config('SECRET_KEY')
 SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.getenv("debug", default=False)
 
 ALLOWED_HOSTS = ['*']
 
